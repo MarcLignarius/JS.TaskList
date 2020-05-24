29500f0f79dc5e48db16151c034a6095fba93f04
Marc Davies <marcdaviesriot@gmail.com>
2020-05-23 17:03:44 -0700

    define UI variables

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Define UI Variables
+const form = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task');
+const taskList = document.querySelector('.collection');
+const clearButton = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
