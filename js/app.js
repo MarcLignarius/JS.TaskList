07841b6fca8c8d23f7d4f363c0c44d8c7127e27e
Marc Davies <marcdaviesriot@gmail.com>
2020-05-23 17:17:23 -0700

    add functionality to add a task

--- js/app.js
+++ js/app.js
@@ -1,6 +1,41 @@
-// Define UI Variables
+// Define UI variables
 const form = document.querySelector('#task-form');
 const taskInput = document.querySelector('#task');
 const taskList = document.querySelector('.collection');
 const clearButton = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener('submit', addTask);
+}
+
+// Add Task
+function addTask(e) {
+  if (taskInput.value === '') {
+    alert('Please add a task');
+  }
+  // Create li element
+  const li = document.createElement('li');
+  // Add class
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add class
+  link.className = 'delete-item secondary-content';
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link to li
+  li.appendChild(link);
+  // Append li to ul
+  taskList.appendChild(li);
+  // Clear input
+  taskInput.value = '';
+  e.preventDefault();
+}
