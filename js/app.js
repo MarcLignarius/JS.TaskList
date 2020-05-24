424e9fa48e73bdff1c2b89ad0eb7b78bd3b620f6
Marc Davies <marcdaviesriot@gmail.com>
2020-05-23 17:37:49 -0700

    load tasks from localStorage

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ loadEventListeners();
 
 // Load all event listeners
 function loadEventListeners() {
+  // DOM load event
+  document.addEventListener('DOMContentLoaded', getTasks);
   // Add task event
   form.addEventListener('submit', addTask);
   // Filter task event
@@ -20,7 +22,35 @@ function loadEventListeners() {
   clearButton.addEventListener('click', clearTasks);
 }
 
-// Add Task
+// Get tasks from localStorage
+function getTasks() {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.forEach(function (task) {
+    // Create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(task));
+    // Create new link element
+    const link = document.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append the link to li
+    li.appendChild(link);
+    // Append li to ul
+    taskList.appendChild(li);
+  });
+}
+
+// Add task
 function addTask(e) {
   if (taskInput.value === '') {
     alert('Please add a task');
@@ -41,9 +71,23 @@ function addTask(e) {
   li.appendChild(link);
   // Append li to ul
   taskList.appendChild(li);
+  // Store in local storage
+  storeTaskInLocalStorage(taskInput.value);
   // Clear input
   taskInput.value = '';
   e.preventDefault();
+}
+
+// Store task
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Filter tasks
